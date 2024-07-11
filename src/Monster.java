d4cf925f2cd2b66d9f7ac5400ff8c645aa10aba8
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-11 14:52:09 -0400

    1

---
+++ src/Monster.java
@@ -0,0 +1,12 @@
+public class Monster {
+    public String name;
+
+    Monster(String name){
+        this.name = name;
+    }
+
+    String attack(){
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+
+}
